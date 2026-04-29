f2af42f29ea01052114e8bb7f02a33245ba66deb
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-04-29 17:42:52 +0900

    Fix boot.rb logger check syntax
    - Use safe navigation operator (&.) instead of blank? method
    - Fix ARGV check logic for server detection

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 # Set up RAILS_LOG_TO_STDOUT automatically in development if running as server
-if ENV['RAILS_LOG_TO_STDOUT'].blank? && ARGV.include?('s') || ARGV.include?('server')
+if !ENV['RAILS_LOG_TO_STDOUT']&.present? && (ARGV.include?('s') || ARGV.include?('server'))
   ENV['RAILS_LOG_TO_STDOUT'] = 'true'
 end
 
